d5f60028c7d57ff984f85d9602d1236be63b6017
Archit-D2K <i-archit.rivastava@d2kindia.com>
2024-12-22 18:59:07 +0530

    update database url

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,8 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-
+import 'dotenv/config';
 const pool = new Pool({
-  connectionString: "postgresql://attendance-tracker_owner:vnrWQPiJL90G@ep-tight-bonus-a58s11f0-pooler.us-east-2.aws.neon.tech/attendance-tracker?sslmode=require",
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
